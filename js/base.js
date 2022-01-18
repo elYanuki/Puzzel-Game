1d70ea839296f2744797fdb13debf6a611dcd7f4
elYanuki <pc.mail.yanik@gmail.com>
2022-01-18 20:26:54 +0100

    formatted code and addet discriptions

--- js/base.js
+++ js/base.js
@@ -1,20 +1,24 @@
 let position = [2,2]//[row][column]
-let maxWidth
+let data
+
+loadLevel(1)
 
 function loadLevel(index){
-    let data = lvl1()
-    document.getElementById("test").innerHTML = `${data[1][1]}`
-
-    maxWidth = data[1].length
+    switch (index){
+        case 1: data = lvl1();break;
+        case 2: data = lvl2();break;
+        case 3: data = lvl3();break;
+        case 4: data = lvl4();break;
+    }
     
-    document.getElementById("player").style = `grid-area: ${data[data.length-1][0]} / ${data[data.length-1][1]} / auto / auto;`
-    position = [data[data.length-1][0],data[data.length-1][1]]
+    document.getElementById("player").style = `grid-area: ${data[data.length-1][0]} / ${data[data.length-1][1]} / auto / auto;` //setzt startpos des character auf coordinaten in letzter zeile der data
+    position = [data[data.length-1][0],data[data.length-1][1]] //wie oben nur für gespeicherte pos
 
     let gridItems = ""
-    for (let i = 0; i < data.length-2; i++) {
+    for (let i = 0; i < data.length-2; i++) { //lässt breite des array die breite des grid bestimmen
         gridItems += " 1fr"
     }
-    document.getElementById("board").style = `grid-template-columns:${gridItems};grid-template-rows:${gridItems};`
+    document.getElementById("board").style = `grid-template-columns:${gridItems};grid-template-rows:${gridItems};` //setzt grid breite und höhe
 }
 
 function movePlayer(direction){
@@ -25,19 +29,19 @@ function movePlayer(direction){
             position[0] = Math.max(--position[0], 1) //1 damit nicht aus dem feld läuft
             break
         case 1: //down s
-            position[0] = Math.min(++position[0], maxWidth)
+            position[0] = Math.min(++position[0], data[1].length)
             break
         case 2: //left a 
             position[1] = Math.max(--position[1], 1)
             break
         case 3: //right d 
-            position[1] = Math.min(++position[1], maxWidth)
+            position[1] = Math.min(++position[1], data[1].length)
             break
     }
 
 
     player.style = `grid-area: ${position[0]} / ${position[1]} / auto / auto;`
-    console.log(position[0] + " " + position[1]);
+    console.log("coordinates: r:" + position[0] + " c:" + position[1]);
 }
 
 //Select Keys for Movement
--- js/levelData.js
+++ js/levelData.js
@@ -1,5 +1,3 @@
-console.log("Hallo")
-
 function lvl1(){
 let levelTemplate = [
     ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ],

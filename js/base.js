cc345462044be852f4b5d4df80ce296a27b57055
elYanuki <pc.mail.yanik@gmail.com>
2022-01-18 18:37:07 +0100

    Actually fixed movement script
    ändert pos im grid nicht mehr mit player. grid colum sondern speichert werte im array und ändert danach player. style = "grid-area:*werte*"

--- js/base.js
+++ js/base.js
@@ -1,31 +1,35 @@
 document.getElementById("player").style =  "grid-row: 2;grid-column: 2;"
+let position = [2,2]//[row][column]
+let maxWidth = 12
 
-function moveplayer(direction){
+function movePlayer(direction){
     let player = document.getElementById("player")
 
     switch (direction){
-        case 0: //up
-            player.style.gridRow = Math.max(--player.style.gridRow, 1)
+        case 0: //up w
+            position[0] = Math.max(--position[0], 1) //1 damit nicht aus dem feld läuft
             break
-        case 1: //down
-            player.style.gridRow = Math.min(++player.style.gridRow, 12)
+        case 1: //down s
+            position[0] = Math.min(++position[0], maxWidth)
             break
-        case 2: //left
-            player.style.gridColumn = Math.max(--player.style.gridColumn, 1)
+        case 2: //left a 
+            position[1] = Math.max(--position[1], 1)
             break
-        case 3: //right
-            player.style.gridColumn = Math.min(++player.style.gridColumn, 12)
+        case 3: //right d 
+            position[1] = Math.min(++position[1], maxWidth)
             break
     }
-    //yaniks test comment
+
+    player.style = `grid-area: ${position[0]} / ${position[1]} / auto / auto;`
+    console.log(position[0] + " " + position[1]);
 }
 
 document.addEventListener('keydown', function(event) {
     console.log(event.key);
         switch(event.key){
-            case 'w': moveplayer(0);break
-            case 's': moveplayer(1);break
-            case 'a': moveplayer(2);break
-            case 'd': moveplayer(3);break
+            case 'w': movePlayer(0);break;
+            case 's': movePlayer(1);break;
+            case 'a': movePlayer(2);break;
+            case 'd': movePlayer(3);break;
     }
 });

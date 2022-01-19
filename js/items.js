45e77d455471cd7d8c1b1b28a57785f545fd1a28
elYanuki <pc.mail.yanik@gmail.com>
2022-01-19 15:57:35 +0100

    fixed movement and portals
    bug, dass man durch den ersten block geht fixed.
    bug, teleporter in alle richtungen verlassen fixed
    collision detection fixed
    added setPlayerPos method

--- js/items.js
+++
@@ -1,6 +0,0 @@
-function portale(nextY, nextX){
-    position[0] = data[nextY-1][nextX-1].substr(2,2)
-    position[1] = data[nextY-1][nextX-1].substr(4,2)
-    document.getElementById("player").style = `grid-area: ${data[nextY-1][nextX-1].substr(2,2)} / ${data[nextY-1][nextX-1].substr(4,2)} / auto / auto;` //setzt startpos des character auf coordinaten in letzter zeile der data
-    fixPosition(data[nextY-1][nextX-1].substr(2,2), data[nextY-1][nextX-1].substr(4,2));
-}

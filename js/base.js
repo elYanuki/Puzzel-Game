76a63a1faf4aff29cf94a57d7ea71523279f88ac
elYanuki <pc.mail.yanik@gmail.com>
2022-01-20 12:43:20 +0100

    Animations and better ghost
    ghost timer bar
    popup animation
    tp animation

--- js/base.js
+++ js/base.js
@@ -2,9 +2,12 @@ let position = [2,2]//[row][column]
 let data
 let effect = "normal"
 let eDown = false
+let noMove = false
 
 loadLevel(1)
 setElements();
+
+let player = document.getElementById("player")
 
 function loadLevel(index){
     switch (index){
@@ -13,6 +16,9 @@ function loadLevel(index){
         case 3: data = lvl3();break;
         case 4: data = lvl4();break;
     }
+
+    effect = "normal"
+    noMove = "false"
 
     document.getElementById("player").style = `grid-area: ${data[data.length-1][0]} / ${data[data.length-1][1]} / auto / auto;` //setzt startpos des character auf coordinaten in letzter zeile der data
     position = [data[data.length-1][0],data[data.length-1][1]] //wie oben nur für gespeicherte pos
@@ -25,10 +31,7 @@ function loadLevel(index){
 }
 
 function movePlayer(direction){
-    let player = document.getElementById("player")
     let move = false;
-
-
     let pos0 = position[0]
     let pos1 = position[1]
 
@@ -61,14 +64,17 @@ function movePlayer(direction){
 
 
     player.style = `grid-area: ${position[0]} / ${position[1]} / auto / auto;`
-    console.log("coordinates: r:" + position[0] + " c:" + position[1]);
+    /* console.log("coordinates: r:" + position[0] + " c:" + position[1]); */
 
     checkPosition();
 }
 
 //Diese Funktion schaut ob der nächste Schritt von dem Spieler möglich ist
 function checkMove(nextY, nextX){
-    if(data[nextY-1][nextX-1]){
+    if(noMove == true){
+        return false
+    }
+    else if(data[nextY-1][nextX-1]){
         //Next Position is a Wall
         if(data[nextY-1][nextX-1].substr(0,2) == "wa"){
             return false;
@@ -88,12 +94,12 @@ function checkMove(nextY, nextX){
         //Next Position is a Trap
         if(data[nextY-1][nextX-1].substr(0,2) == "tr"){
             trap();
-            return false;
+            return true;
         } 
         //Next Position is a Portal
         if(data[nextY-1][nextX-1].substr(0,2) == "po"){
             portale(nextY, nextX);
-            return false;
+            return true;
         } 
         //Next Position is a Softwall
         if(data[nextY-1][nextX-1].substr(0,2) == "sw"){
@@ -110,20 +116,53 @@ function checkMove(nextY, nextX){
     }
 }
 
-//Diese Funktion schaut ob ein Item auf dem Feld auf dem der Spielr steht, liegt
+//schaut ob ein Item auf dem Feld auf dem der Spielr steht, liegt
 function checkPosition(){
     let posPlayer = data[position[0]-1][position[1]-1]
-    if(posPlayer){
-        createInfoPopup(position[0], position[1]);
+
+    if (posPlayer.substr(0, 2) == "sw" && effect != "ghost") {
+        dead()
+    }
+
+    if(posPlayer == "gh"){ //muss alle eigenschafften haben damit man nicht in der softwall ein popup bekommt
+
+        createInfoPopup(position[0], position[1],);
+
         if (eDown == true) {
-            clearInfoPopup()
+
+            clearInfoPopup() //e sorgt ja für den pickup
+
             if (posPlayer.substr(0, 2) == "gh") {
-                ghost.parentNode.removeChild(ghost);
+                let ghost = document.getElementById("ghost")
+                ghost.style.width = 0
+                ghost.style.height = 0
+
+                setTimeout(function () {
+                    ghost.parentNode.removeChild(ghost);        
+                }, 500)
+
                 effect = "ghost";
                 data[position[0] - 1][position[1] - 1] = "";
 
+                let softwalls = document.getElementsByClassName("softwall")
+
+                player.classList.add("player-ghostmode")
+
+                for (let i = 0; i < softwalls.length; i++) {
+                    softwalls[i].classList.add("softwall-ghostmode")
+                }
+
+                countdown(10, "Ghost")
+
                 setTimeout(function () {
                     effect = "normal"
+
+                    player.classList.remove("player-ghostmode")
+
+                    for (let i = 0; i < softwalls.length; i++) {
+                        softwalls[i].classList.remove("softwall-ghostmode")
+                    }
+                    checkPosition()
                 }, 10000)
             }
         }
@@ -144,19 +183,19 @@ function setElements(){
             if(data[i][j]){
                 let sub = data[i][j].substr(0,2)
                 switch(sub){
-                    case ("wa"): document.getElementById("board").innerHTML += `<div style="grid-area: ${i+1} / ${j+1} / auto / auto; background-color: rgb(0, 41, 128);" class="blocks"></div>`
+                    case ("wa"): document.getElementById("board").innerHTML += `<div style="grid-area: ${i+1} / ${j+1} / auto / auto; background-color: rgb(0, 41, 128);" class="block"></div>`
                                     //thing.style = `backgroundImage: url(block.png);`  
                                     break;
-                    case ("tr"): document.getElementById("board").innerHTML += `<div style="grid-area: ${i+1} / ${j+1} / auto / auto; background-color: rgb(200, 100, 28);" class="blocks"></div>`
+                    case ("tr"): document.getElementById("board").innerHTML += `<div style="grid-area: ${i+1} / ${j+1} / auto / auto; background-color: rgb(200, 100, 28);" class="block"></div>`
                                     //thing.style = `backgroundImage: url(block.png);`  
                                     break;
-                    case ("po"): document.getElementById("board").innerHTML += `<div style="grid-area: ${i+1} / ${j+1} / auto / auto; background-color: rgb(0, 0, 0);" class="blocks"></div>`
+                    case ("po"): document.getElementById("board").innerHTML += `<div style="grid-area: ${i+1} / ${j+1} / auto / auto; background-color: rgb(0, 0, 0);" class="block"></div>`
                                     //thing.style = `backgroundImage: url(block.png);`  
                                     break;
-                    case ("sw"): document.getElementById("board").innerHTML += `<div style="grid-area: ${i+1} / ${j+1} / auto / auto; background-color: rgb(0, 80, 128);" class="things"></div>`
+                    case ("sw"): document.getElementById("board").innerHTML += `<div style="grid-area: ${i+1} / ${j+1} / auto / auto; background-color: rgb(0, 80, 128);" class="softwall"></div>`
                                     //thing.style = `backgroundImage: url(block.png);`  
                                     break;
-                    case ("gh"): document.getElementById("board").innerHTML += `<div id="ghost" style="grid-area: ${i+1} / ${j+1} / auto / auto; background-color: rgb(255, 255, 255);" class="things"></div>`
+                    case ("gh"): document.getElementById("board").innerHTML += `<div id="ghost" style="grid-area: ${i+1} / ${j+1} / auto / auto; background-color: rgb(255, 255, 255);" class="block"></div>`
                                     //thing.style = `backgroundImage: url(block.png);`  
                                     break;
                 }
@@ -173,20 +212,50 @@ function createInfoPopup(x,y,text) {
         document.getElementById("info-text").innerHTML = `Press E to pick up the item`
     }
     document.getElementById("info-relative").style = `grid-area: ${x} / ${y} / auto / auto;visibility:visible;`
+    document.getElementById("info").style.width = "10rem"
+    document.getElementById("info").style.opacity = "1"
+    document.getElementById("info-arrow").style.opacity = "1"
+
+    setTimeout(function () {
+        document.getElementById("info-text").style.opacity = "1"
+    }, 300)
+    
 }
 
 function clearInfoPopup(){
-    document.getElementById("info-relative").style = `visibility:hidden`
+    document.getElementById("info-text").style.opacity = "0"
+    document.getElementById("info").style.width = "0rem"
+    document.getElementById("info").style.opacity = "0"
+    document.getElementById("info-arrow").style.opacity = "0"
+    document.getElementById("info-relative").style.visibility = "hidden"
+}
+
+function countdown(duration, name){
+    document.getElementById("countdown").style = `animation-name: countdown; animation-duration: ${duration}s;`
+    document.getElementById("countdown-text").innerHTML = name;
+
+    setTimeout(function () {
+        document.getElementById("countdown-text").innerHTML = ""
+    }, duration*1000)
 }
 
 //Select Keys for Movement
 document.addEventListener('keydown', function(event) {
         switch(event.key){
-            case 'w': movePlayer(0);break;
-            case 's': movePlayer(1);break;
-            case 'a': movePlayer(2);break;
-            case 'd': movePlayer(3);break;
-            case 'e': eDown = true; checkPosition();break
+            case 'w':
+            case 'ArrowUp':
+            case 'W': movePlayer(0);break;
+            case 's':
+            case 'ArrowDown':
+            case 'S': movePlayer(1);break;
+            case 'a':
+            case 'ArrowLeft':
+            case 'A': movePlayer(2);break;
+            case 'd':
+            case 'ArrowRight':
+            case 'D': movePlayer(3);break;
+            case 'e':   
+            case 'E': eDown = true; checkPosition();break
     }
 });
 
@@ -197,12 +266,46 @@ document.addEventListener('keyup',function(event) {
 });
 
 function dead(){
-    loadLevel(1);
+    effect = "normal"
+
+    player.classList.remove("player-ghostmode")
+
+    let softwalls = document.getElementsByClassName("softwall")
+
+    for (let i = 0; i < softwalls.length; i++) {
+        softwalls[i].classList.remove("softwall-ghostmode")
+    }
+    checkPosition()
+
+    document.getElementById("countdown-text").innerHTML = ""
+    document.getElementById("countdown").style = `animation-name: none;`
+    noMove = true
+
+    setTimeout(function () {
+        player.style.visibility = "hidden"
+    }, 200)
+    setTimeout(function () {
+        player.style.visibility = "visible"
+    }, 400)
+    setTimeout(function () {
+        player.style.visibility = "hidden"
+    }, 600)
+    setTimeout(function () {
+        player.style.visibility = "visible"
+    }, 800)
+    setTimeout(function () {
+        player.style.visibility = "hidden"
+    }, 1000)
+
+    setTimeout(function () {
+        player.style.visibility = "visible"
+        loadLevel(1);
+    }, 1200)
 }
 
 function SetPlayerPos(row,column){
     position[0] = row
     position[1] = column
     
-    document.getElementById("player").style = `grid-area: ${row} / ${column} / auto / auto;`
+    player.style = `grid-area: ${row} / ${column} / auto / auto;`
 }
--- js/blocks.js
+++ js/blocks.js
@@ -4,8 +4,22 @@ function trap(){
 }
 
 function portale(nextY, nextX){
+    noMove = true
+    setTimeout(function () {
+        player.style.width = "90%"
+        player.style.height = " 90%"
+    }, 10)
+    
+    setTimeout(function () {
+        player.style.width = "0%"
+        player.style.height = " 0%"
+    }, 200)
+
+    setTimeout(function () {
     position[0] = parseInt(data[nextY-1][nextX-1].substr(3,2))
     position[1] = parseInt(data[nextY-1][nextX-1].substr(6,2))
 
-    document.getElementById("player").style = `grid-area: ${position[0]} / ${position[1]} / auto / auto;`
+    player.style = `grid-area: ${position[0]} / ${position[1]} / auto / auto;`
+    }, 400)
+    noMove = false
 }
